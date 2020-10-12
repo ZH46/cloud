60d113d59ee98b86da715473b3568d0120bdfbe0
ZH46 <769050668@qq.com>
2020-10-12 09:16:39 +0800

    eureka集群

---
+++ cloud-eureka-server-7002/src/main/java/com/zh/springcloud/EurekaApplication7002.java
@@ -0,0 +1,17 @@
+package com.zh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 卓少武
+ * @date 2020/10/11
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication7002.class,args);
+    }
+}
